fef051c5e44d0498e4040820784a8a8a4f24d98d
myat555 <myat-555@hotmail.com>
2023-06-13 11:52:48 -0700

    new changes

--- youtube-clone/src/App.tsx
+++ youtube-clone/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Watch from './pages/Watch'
+import Search from './pages/Search'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
